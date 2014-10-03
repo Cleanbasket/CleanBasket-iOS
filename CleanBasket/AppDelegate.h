70e956470ba8b8dc7310bc5e895f55890e32ab8e
woonohyo <woonohyo@nhnnext.org>
2014-10-03 15:47:32 +0900

    Initial Commit

---
+++ CleanBasket/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  CleanBasket
+//
+//  Created by Wonhyo Yi on 2014. 10. 3..
+//  Copyright (c) 2014년 WashAppKorea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ CleanBasket/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CleanBasket
+//
+//  Created by Wonhyo Yi on 2014. 10. 3..
+//  Copyright (c) 2014년 WashAppKorea. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
