42fd04151f31a06ded9e160a816bd94be884ec53
lsj333610dl <lsj333610dl@naver.com>
2015-12-07 17:34:16 +0900

    Initial Commit

---
+++ CleanBasket/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CleanBasket
+//
+//  Created by 이상진 on 2015. 12. 7..
+//  Copyright © 2015년 WashappKorea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CleanBasket/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  CleanBasket
+//
+//  Created by 이상진 on 2015. 12. 7..
+//  Copyright © 2015년 WashappKorea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
