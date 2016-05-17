66ff20d15679307615dee0598906e328dc460908
ChaYongbin <theodore.cha@cleanbasket.co.kr>
2016-05-17 14:11:24 +0900

    version update commit

---
+++ CleanBasket/View/RoundMint.h
@@ -0,0 +1,13 @@
+//
+//  RoundMint.h
+//  CleanBasket
+//
+//  Created by ChaTheodore on 2016. 5. 12..
+//  Copyright © 2016년 WashappKorea. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RoundMint : NSObject
+
+@end
---
+++ CleanBasket/View/RoundMint.m
@@ -0,0 +1,13 @@
+//
+//  RoundMint.m
+//  CleanBasket
+//
+//  Created by ChaTheodore on 2016. 5. 12..
+//  Copyright © 2016년 WashappKorea. All rights reserved.
+//
+
+#import "RoundMint.h"
+
+@implementation RoundMint
+
+@end
